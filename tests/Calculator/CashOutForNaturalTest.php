0c9ef74dfea6f10dbdd64ec861d84aa13f26f93e
Viktor Robev <viktorrobev@gmail.com>
2020-02-03 21:19:43 +0200

    Add a testcase for CashOutForNaturalTest

--- tests/Calculator/CashOutForNaturalTest.php
+++ tests/Calculator/CashOutForNaturalTest.php
@@ -175,7 +175,7 @@ class CashOutForNaturalTest extends TestCase
         $this->assertMoneyAreEqual($expectedCommission, $actualCommission);
     }
 
-    public function testCalculateCommissionForLegalShouldBeZero()
+    public function testCalculateCommissionForLegalMustBeZero()
     {
         $expectedCommission = new Money(0, Currency::EUR);
 
@@ -184,6 +184,34 @@ class CashOutForNaturalTest extends TestCase
         $operation->amount = new Money(100000, Currency::EUR);
         $operation->type = Operation::TYPE_CASH_OUT;
         $operation->userType = Operation::USER_TYPE_LEGAL;
+        $operation->userId = 1;
+
+        $this->operationStorageMock->expects($this->any())
+            ->method('getCashOutOperationsNumberForLastCalendarWeekForUser')
+            ->with($operation->userId, $operation->date)
+            ->willReturn(2);
+
+        $this->operationStorageMock->expects($this->any())
+            ->method('getCashOutOperationsValueForLastCalendarWeekForUserInEur')
+            ->with($operation->userId, $operation->date)
+            ->willReturn(new Money(0, Currency::EUR));
+
+        $initialCommission = new Money(0, Currency::EUR);
+
+        $actualCommission = $this->commissionCalculator->calculateCommission($operation, $initialCommission);
+
+        $this->assertMoneyAreEqual($expectedCommission, $actualCommission);
+    }
+
+    public function testCalculateCommissionForCashInMustBeZero()
+    {
+        $expectedCommission = new Money(0, Currency::EUR);
+
+        $operation = new Operation();
+        $operation->date = new \DateTimeImmutable('2020-02-03');
+        $operation->amount = new Money(100000, Currency::EUR);
+        $operation->type = Operation::TYPE_CASH_IN;
+        $operation->userType = Operation::USER_TYPE_NATURAL;
         $operation->userId = 1;
 
         $this->operationStorageMock->expects($this->any())
